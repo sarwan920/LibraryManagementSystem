763960d60c8b93f39ddb21922c57e705687f2b06
Sarwan Nizamani <Sarwan Nizamani>
2019-04-19 17:06:29 +0500

    some changes to Book.java and createadmin file

--- LibraryManagementSystem/src/Book.java
+++ LibraryManagementSystem/src/Book.java
@@ -1,9 +1,7 @@
 public class Book
 {
-  
-  public static void main(String [] args)
-  {
+	public static void main(String []args)
+	{
   System.out.println("Added this class to add books to library");
-    System.out.println("some addition to code of this file");
-  }
+	}
 }
--- LibraryManagementSystem/src/CreateAdminAccount.java
+++ LibraryManagementSystem/src/CreateAdminAccount.java
@@ -1,6 +1,6 @@
 public class CreateAdminAccount
 {
-    public static void main(String []args
+    public static void main(String []args)
     {
       //this class we have created to  make account for admin
     }
